44dea6a7687542e4b0ea552b1c2b919e815b0ba6
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-01 13:42:55 +0200

    fixed a syntax error{ where everything was supposed to be in inventory braces

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,38 +1,38 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Inventory {
+
     private List<Item> laptops = new ArrayList<>();
 
     public void addLaptop(Item laptop) {
         laptops.add(laptop);
-        System.out.println("laptop added to inventory.");
+        System.out.println("Laptop added to inventory.");
     }
 
     public void removeLaptop(int id) {
         for (Item laptop : laptops) {
             if (laptop.getId() == id) {
                 laptops.remove(laptop);
-                System.out.println("Laptop Removed.");
+                System.out.println("Laptop removed.");
                 return;
             }
-
         }
         System.out.println("Laptop not found.");
     }
 
-}
-public void updateLaptop(int id, int quantity, double price) {
-    for (Item laptop : laptops) {
-        if (laptop.getId() == id) {
-            laptop.setQuantity(quantity);
-            laptop.setPrice(price);
-            System.out.println("Laptop updated.");
-            return;
+    public void updateLaptop(int id, int quantity, double price) {
+        for (Item laptop : laptops) {
+            if (laptop.getId() == id) {
+                laptop.setQuantity(quantity);
+                laptop.setPrice(price);
+                System.out.println("Laptop updated.");
+                return;
+            }
         }
+        System.out.println("Laptop not found.");
     }
-    System.out.println("Laptop not found.");
+
     public void displayLaptops() {
 
         if (laptops.isEmpty()) {
@@ -50,5 +50,3 @@ public void updateLaptop(int id, int quantity, double price) {
         }
     }
 }
-
-
