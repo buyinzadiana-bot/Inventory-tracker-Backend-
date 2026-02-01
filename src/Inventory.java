9927aaa46654db9c39ecd2c5a73493ec40396589
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-01 13:34:26 +0200

    remove laptop to the inventory feature

--- src/Inventory.java
+++ src/Inventory.java
@@ -9,6 +9,18 @@ public class Inventory {
         laptops.add(laptop);
         System.out.println("laptop added to inventory.");
     }
+
+    public void removeLaptop(int id) {
+        for (Item laptop : laptops) {
+            if (laptop.getId() == id) {
+                laptops.remove(laptop);
+                System.out.println("Laptop Removed.");
+                return;
+            }
+
+        }
+        System.out.println("Laptop not found.");
+    }
 }
 
 
